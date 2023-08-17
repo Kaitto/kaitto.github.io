bacb94ca1ce7ecdb8a846670bfcf5b9c03506899
Kaitto <kaittoneko@gmail.com>
2023-08-17 00:00:22 -0300

    Incorporando pruebas y elementos útiles para el diseño de la pagina *   Adquirir informacion con ajax y actualizar el dom *   Mostrar y ocultar elementos segun el dispositivo *   Incorporar iconos svg

--- js/main.js
+++ js/main.js
@@ -6,9 +6,27 @@ $.ajax({
     url: 'https://randomuser.me/api/',
     dataType: 'json',
     success: function(data) {
+        //Al recibir los datos, ejecutar esta funcion.
       console.log(data);
+      if (data.results.length > 0) {
+        let name=data.results[0].name;
+        $("#nombre").html(`${name.title} ${name.first} ${name.last}`);
+        $("#img_perfil").attr("src",data.results[0].picture.medium);
+        $("#img_perfil").attr("hidden",false);
+      } else {
+        console.warn("No hay resultados desde randomuser.me");
+      }
+      //$("nombre").html=data.name
+      //data.name
+    },
+    error: function(xhr, status, error) {
+        //Si ocurre algun problema al generar/recibir los datos, ejecuta esto.
+      console.warn(status)
     }
 });
+
+
+
 
 
      
